0f6a3f7382eef725f970fc7b9f4a928c7dbbc2fb
jesus.leo0508 <jesus.leo.0508@gmail.com>
2020-04-27 17:19:13 +0000

    Agregue modelatriubte

--- src/main/java/pe/com/bn/controller/LoginController.java
+++ src/main/java/pe/com/bn/controller/LoginController.java
@@ -1,6 +1,8 @@
 package pe.com.bn.controller;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -9,14 +11,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.com.bn.domain.Country;
 import pe.com.bn.domain.User;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @ModelAttribute("countries")
+    public List<Country> countries(){
+        return Arrays.asList(new Country(1, "Argentina"),
+                        new Country(2, "Bolivia"),
+                        new Country(3, "Brasil"),
+                        new Country(4, "Chile"),
+                        new Country(5, "Colombia"),
+                        new Country(6, "Ecuador"),
+                        new Country(7, "Paraguay"),
+                        new Country(8, "Perú"),
+                        new Country(9, "Uruguay"),
+                        new Country(10, "Venezuela") );
+    }
 
 	@GetMapping({ "/", "" })
 	public String index(Model model) {
